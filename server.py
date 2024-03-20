710f2b7366fb0a1b229d4dcf4326762ba16df906
Munmi Baruah <munmi@positra.com>
2024-03-20 20:46:26 +0530

    adding pylint ignore file

--- server.py
+++ server.py
@@ -1,21 +1,28 @@
-from flask import Flask, request, jsonify, render_template
+"""
+Final project: AI-Based Web Application Development and Deployment
+"""
+# Import flask, request and render_template from flask
+from flask import Flask, request, render_template
+
+# Import emotion_detector from EmotionDetection
 from EmotionDetection.emotion_detection import emotion_detector
 
+# Initiate the flask app
 app = Flask(__name__)
 
 @app.route("/")
 def get_emotion():
+    """ Render index.html template in the base url """
     return render_template("index.html")
 
 @app.route("/emotionDetector")
 def sent_detector():
+    """ emotion detector """
     text_to_analyze = request.args.get('textToAnalyze')
     emotions = emotion_detector(text_to_analyze)
-
     if emotions is None:
         return "Invalid text! Please try again!."
-    else:
-        return "The given text has been identified as {}.".format(emotions)
+    return "The given text has been identified as {}.".format(emotions)
 
 if __name__ == "__main__":
     app.run(debug=True)
